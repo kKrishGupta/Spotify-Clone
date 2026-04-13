8447fed8fd7a34db25532f35b306ddc14467a314
kKrishGupta <kg20060220@gmail.com>
2026-04-13 15:49:21 +0530

    login and register routes and controller created

--- Backend/server.js
+++ Backend/server.js
@@ -3,8 +3,17 @@ require('dotenv').config();   // ✅ load first
 const app = require('./src/app');
 const port = process.env.PORT || 3000;
 const connectDB = require('./src/config/db');
-connectDB();
 
-app.listen(port, () => {
-  console.log(`server is running on http://localhost:${port}`);
-});
+// ❗ FIX: make DB connection awaited
+connectDB()
+  .then(() => {
+    console.log("DataBase connect successfully");
+
+    app.listen(port, () => {
+      console.log(`server is running on http://localhost:${port}`);
+    });
+
+  })
+  .catch((err) => {
+    console.error("DB connection failed:", err);
+  });

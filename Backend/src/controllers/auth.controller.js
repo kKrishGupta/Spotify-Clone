d55563aa128729a6c937eb5031b74e8c9d357361
kKrishGupta <kg20060220@gmail.com>
2026-04-13 14:33:35 +0530

    register and login structure

---
+++ Backend/src/controllers/auth.controller.js
@@ -0,0 +1,12 @@
+const userModel = require('./models/user.models');
+const jwt = require('jsonwebtoken');
+  
+  async function registerUser(req,res){
+
+  }
+  async function loginUser (req,res){
+
+  }
+module.exports = {
+  registerUser, loginUser
+};
